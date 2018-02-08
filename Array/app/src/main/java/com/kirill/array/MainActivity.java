a6a053a93da947af083fcf79a085b239242fa29a
Kirill <kirill.herz@gmail.com>
2018-02-08 17:00:53 +0300

    Внесены исправления

--- Array/app/src/main/java/com/kirill/array/MainActivity.java
+++ Array/app/src/main/java/com/kirill/array/MainActivity.java
@@ -1,24 +1,39 @@
 package com.kirill.array;
 
+import android.graphics.Color;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
+
+import static android.graphics.Color.*;
 
 public class MainActivity extends AppCompatActivity {
     TextView textViewCapacity;
     TextView textViewSize;
     TextView textViewResult;
+
     EditText editTextCapacity;
     EditText editTextIndex;
     EditText editTextValue;
+
     Button buttonCapacity;
     Button buttonExecute;
+
     ProgressBar progressBarCapacity;
+
     Array<String> array;
+
+    RadioButton radioButtonAdd;
+    RadioButton radioButtonDelete;
+    RadioButton radioButtonChange;
+    RadioGroup radioGroup;
 
 
     @Override
@@ -28,13 +43,24 @@ public class MainActivity extends AppCompatActivity {
         textViewCapacity = (TextView) findViewById(R.id.textViewCapacity);
         textViewSize = (TextView) findViewById(R.id.textViewSize);
         textViewResult = (TextView) findViewById(R.id.textViewResult);
+
         editTextCapacity = (EditText) findViewById(R.id.editTextCapacity);
         editTextIndex = (EditText) findViewById(R.id.editTextIndex);
         editTextValue = (EditText) findViewById(R.id.editTextValue);
+
         buttonCapacity = (Button) findViewById(R.id.buttonCapacity);
         buttonExecute = (Button) findViewById(R.id.buttonExecute);
+
         progressBarCapacity = (ProgressBar) findViewById(R.id.progressBarCapacity);
+
+
+        radioButtonAdd = (RadioButton) findViewById(R.id.radioButtonAdd);
+        radioButtonChange = (RadioButton) findViewById(R.id.radioButtonChange);
+        radioButtonDelete = (RadioButton) findViewById(R.id.radioButtonDelete);
+        radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
+
         array = new Array<>();
+
         update();
 
     }
@@ -47,13 +73,45 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    public void buttonExecuteOnClick(View view) {
-        int index = Integer.parseInt(editTextIndex.getText().toString());
+    public void add(View view) {
         String value = editTextValue.getText().toString();
-        array.add(index, value);
+        int index = 0;
+        try {
+            index = Integer.parseInt(editTextIndex.getText().toString());
+            array.add(index, value);
+            update();
+            editTextIndex.setText(String.valueOf(index + 1));
+            editTextValue.setText("");
+            textViewResult.setTextColor(Color.GREEN);
+            textViewResult.setText("Добавлено значение: " + value + "\nпо индексу: " + index);
+        } catch (NumberFormatException e) {
+            textViewResult.setTextColor(Color.RED);
+            textViewResult.setText("Вы ввели неккоректный индекс");
+        } catch (IndexOutOfBoundsException e) {
+            textViewResult.setTextColor(Color.RED);
+            textViewResult.setText("Вы вышли за границы массива");
+        }
+
+    }
+
+    public void delete(View view) {
+        int index = Integer.parseInt(editTextIndex.getText().toString());
+        String value = array.delete(index);
+        textViewResult.setTextColor(Color.GREEN);
+        textViewResult.setText("Удалено значение: " + value + "\nпо индексу: " + index);
         update();
-        editTextIndex.setText(String.valueOf(index + 1));
-        editTextValue.setText("");
-        textViewResult.setText("Добавлено значение: " + value + "\nпо индексу: " + index);
+    }
+
+    public void dispatchOnClick(View view) {
+        switch (radioGroup.getCheckedRadioButtonId()) {
+            case R.id.radioButtonAdd:
+                add(view);
+                break;
+            case R.id.radioButtonDelete:
+                delete(view);
+                break;
+            default:
+                break;
+        }
     }
 }
